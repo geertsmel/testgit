96e2d4744f6dbe51ef6a8f4a546fb5d06f93814c
Melanie Geerts <geerts.m@hotmail.com>
2021-01-09 11:38:47 +0100

    commit 2

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("coucou");
         System.out.println("coucou3");
-        System.out.println("Coucou4);
+        System.out.println("Coucou4");
     }
 }
